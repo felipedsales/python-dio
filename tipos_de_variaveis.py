d387928bcb8c027d0ff6edd657c279bf587e4c7e
felipe <felipe_ssales@hotmail.com>
2024-06-05 21:15:17 -0300

    Adicionando programa com os tipos de variáveis

---
+++ tipos_de_variaveis.py
@@ -0,0 +1,14 @@
+#Texto --> str
+
+#Numérico --> int, float, complex
+
+#Sequência --> list,tuple,range
+
+#Mapa --> dict
+
+#Coleção --> set,frozenset
+
+#Booleano --> bool
+
+#Binário --> bytes, bytearray, memoryview
+
