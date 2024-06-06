4d11bfe1e7da21ce3111f654bde2b162ecab6108
felipe <felipe_ssales@hotmail.com>
2024-06-06 18:40:07 -0300

    git commit -m "Adcionando arquivo de operadores lógicos AND e OR "

---
+++ operadores_logicos.py
@@ -0,0 +1,15 @@
+######################
+# Operadores Lógicos #
+######################
+
+n1 = 100
+
+# Operador and
+
+exemplo_1 = n1 > 99 and n1 < 1000
+print(exemplo_1) 
+
+# Operador or
+
+exemplo_2 = n1 > 101 or n1 < 99
+print(exemplo_2)
