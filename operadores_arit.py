e750e6fea45b9147ec3eb22986b6763e020fb7b8
felipe <felipe_ssales@hotmail.com>
2024-06-05 21:16:46 -0300

    Adicionando programa com os tipos de operadores aritméticos

---
+++ operadores_arit.py
@@ -0,0 +1,47 @@
+##########################
+# Operadores Aritméticos #
+##########################
+
+# Adição
+print(1+1)
+
+# Subtração
+print(2-1)
+
+# Multiplicação
+print(3*4)
+
+# Divisão
+print(12/4)
+
+# Divisão Inteira - Exibe somente resultado com valor inteiro 
+print(11//4)
+
+# Módulo ou Resto da Divisão
+print(10%3)
+
+# Exponenciação
+print(2**3)
+
+## Ordem de Precedência ##
+
+# () -> ** -> * e / -> + e - #
+
+### EXEMPLOS ###
+
+produto_1 = 25
+produto_2 = 12
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 ** produto_2)
+print(produto_1 % produto_2)
+
+x = (10 + 5) * 4
+y = (10 / 2) + 25 * ((2 - 2) ** 2)
+
+print(x)
+print(y)
