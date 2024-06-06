d5619c63a847154312b4b62342e8b325b99e9b13
felipe <felipe_ssales@hotmail.com>
2024-06-06 16:45:28 -0300

    Adicionado arquivo de Operadores de Comparação

---
+++ operadores_comparacao.py
@@ -0,0 +1,24 @@
+################################
+#_# Operadores de Comparação #_#
+################################
+
+n1 = 10
+n2 = 12
+
+# Operador Igual ==
+print(n1 == n2)
+
+# Operador Diferente !=
+print(n1 != n2)
+
+# Operador Maior >
+print(n1 > n2)
+
+# Operador Igual Maior Igual >=
+print(n1 >= n2)
+
+# Operador Igual Menor <
+print(n1 < n2)
+
+# Operador Igual Menor Igual <=
+print(n1 <= n2)
