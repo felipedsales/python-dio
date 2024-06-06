f4a8ff09f32f28e1e625b45fa01f7068adfba00c
felipe <felipe_ssales@hotmail.com>
2024-06-06 18:46:17 -0300

    "Adicionando arquivo com operadores de identidade"

---
+++ operadores_de_identidade.py
@@ -0,0 +1,11 @@
+############################
+# Operadores de Identidade #
+############################
+
+n1 = 100
+n2 = 100
+
+# Operador is
+print(n1 is n2)
+# Operador is not
+print(n1 is not n2)
