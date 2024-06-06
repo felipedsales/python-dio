606a85e5a305680c3505fa41910c44fd8e707e91
felipe <felipe_ssales@hotmail.com>
2024-06-06 17:40:44 -0300

    git commit -m "Adicionando arquivo sobre Operadores de Atribuição"

---
+++ operadores_atribuicao.py
@@ -0,0 +1,34 @@
+############################
+# Operadores de Atribuição #
+############################
+
+n1 = 100
+print(n1)
+
+# Adição
+n1 += 10
+print(n1)
+
+#Subtração
+n1 -= 10
+print(n1)
+
+#Divisão Inteira
+n1 //= 3
+print(n1)
+
+#Divisão
+n1 /= 2
+print(n1)
+
+#Multiplicação
+n1 *= 3
+print(n1)
+
+#Modulação
+n1 %= 2
+print(n1)
+
+#Exponenciação
+n1 **= 2
+print(n1)
