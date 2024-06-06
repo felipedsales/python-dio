8b0c6854ee3a02aa7997e019915cba6d6a340462
felipe <felipe_ssales@hotmail.com>
2024-06-06 18:56:43 -0300

    "Adicionando Operadores de Associação"

---
+++ operadores_de_associacao.py
@@ -0,0 +1,13 @@
+############################
+# Operadores de Associação #
+############################
+
+frutas = ["limao", "laranja", "melancia"]
+curso = ["Curso de Python"]
+
+# Operadores in e not in
+
+print("laranja" not in frutas)
+print("limao" in frutas)
+print("pera" in frutas)
+print("python" in curso) #Este operador é case sensitive
