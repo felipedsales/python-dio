54cd27bdd048a705a20a4069ad8d2c275a47a81b
felipe <felipe_ssales@hotmail.com>
2024-06-12 19:25:50 -0300

    "adicionando documentação no arquivo de conversões e adicionando arquivo de estruturas condicionais"

--- conversoes_de_variaveis.py
+++ conversoes_de_variaveis.py
@@ -1,15 +1,35 @@
+####################################
+# Conversões de tipos de variáveis #
+####################################
+
+# Conversão de float para int
 print(int(1.0))
+
+# Conversão de float para str
 print(str(1.0))
 
+# Conversão de float complexo para int
 print(int(1.97348728))
+
+#Conversão de str para int
 print(int("10"))
+
+#Conversão de str para float
 print(float("10.10"))
+
+# Conversão de int para float
 print(float(100))
 
+#Definição de variável com valor inteiro
 valor = 10
+# Atribuindo valor da variável anterior a nova variável e convertendo para uma string
 valor_str = str(valor)
+# Exibindo o tipo da variável valor
 print(type(valor))
+# Exibindo o tipo da variável valor_str
 print(type(valor_str))
 
+# Realiza a operação de divisão e exibe um valor do tipo float
 print(100 / 2)
+# Realiza a operação de divisão e exibe um valor do tipo inteiro pela inserção das duas /
 print(100 // 2)
