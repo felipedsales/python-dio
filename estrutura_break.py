72731fceb63859ddeecafdfb4ebf9ed85144ef28
felipe <felipe_ssales@hotmail.com>
2024-06-13 23:44:41 -0300

    Adicionando arquivos com exemplos de estrutura com break e continue

---
+++ estrutura_break.py
@@ -0,0 +1,16 @@
+###################
+# Estrutura Break #
+###################
+
+while True:
+    numero = int(input("Digite um número: "))
+
+    if numero == 0:
+        break
+
+# for numero in range(100):
+
+#     if numero % 2 == 0:
+#         continue
+
+#     print(numero, end=" ")
