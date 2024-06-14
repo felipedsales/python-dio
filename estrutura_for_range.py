d30cd52fd54e6708d6129434e89e946fc6be38c2
felipe <felipe_ssales@hotmail.com>
2024-06-13 23:10:50 -0300

    Adicionando arquivo com exemplos de estrutura for in range

---
+++ estrutura_for_range.py
@@ -0,0 +1,10 @@
+##########################
+# Estrutura For in Range #
+##########################
+
+# Funciona com três valores start, stop e step
+for numero in range (0,51,5):
+    print(numero, end=" ")
+# Start -> Inicia em 0 
+# Stop -> Finaliza em 50 
+# Step -> Conta de 5 em 5 
