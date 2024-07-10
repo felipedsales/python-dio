5e912ec99174b9bb117d4a0e6b0271cda514a755
felipe <felipe_ssales@hotmail.com>
2024-07-10 18:56:56 -0300

    Adicionando diretório de estrutura de dados e primeiro arquivo de listas

---
+++ 01-Estruturas de Dados/00_declarando_listas.py
@@ -0,0 +1,23 @@
+#####################
+# Declarando Listas #
+#####################
+
+#Declarando strings dentro de uma lista e exibindo
+frutas = ["maçã", "banana", "pera", "laranja"]
+print(frutas)
+
+#Lista Vazia
+frutas = []
+print(frutas)
+
+#Função list() -> Separa os caracteres por vírgulas 
+letras = list("python")
+print(letras)
+
+#Função list(range()) -> Conta de acordo com o valor informado
+numeros = list(range(10))
+print(numeros)
+
+#Declaração de variáveis diferentes em uma lista
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
